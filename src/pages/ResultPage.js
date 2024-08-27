2aed5579010b2195056cd8a36b31aba304c002c3
Anshul Yadav <predatorakki0906@gmail.com>
2024-08-27 23:39:28 +0530

    added markdown haandling in result page

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { marked } from "marked";
 
 const ResultPage = () => {
 	const { state } = useLocation();
 	const { result } = state || {};
 
+	if (!result) {
+		return <div>No results to display</div>;
+	}
+
+	// Convert the markdown to HTML
+	const htmlContent = marked(result);
+
 	return (
 		<div className="container mx-auto p-8">
 			<h1 className="text-2xl font-bold mb-6">Form Results</h1>
-			<pre>{JSON.stringify(result, null, 2)}</pre>{" "}
-			{/* Stringify the result object */}
+			<div
+				className="formatted-output text-left"
+				dangerouslySetInnerHTML={{ __html: htmlContent }}
+			/>
 		</div>
 	);
 };
